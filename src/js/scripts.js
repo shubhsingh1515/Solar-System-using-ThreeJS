3cfb7a56b1c0ff916c395ba2836d213ef727e3ad
shubham1515 <shubhsingh1515@gmail.com>
2024-10-10 23:22:01 +0530

    Images addes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Import textures
 import starsTexture from "../img/stars.jpg";
-import sunTexture from "../img/sun.jpg";
+import sunTexture from "../img/sun.jpeg";
 import mercuryTexture from "../img/mercury.jpg";
 import venusTexture from "../img/venus.jpg";
 import earthTexture from "../img/earth.jpg";
